2839778fdf5f524179c00e0a6a01d371f4c68760
Emilien-Antoine <emilientoine@gmail.com>
2014-01-16 15:00:46 +0100

    proper overriding of pushelement constructor

--- lib/bud/budoverride.rb
+++ lib/bud/budoverride.rb
@@ -1,5 +1,6 @@
 # List of bud file to override
 require_relative "bud"
+require_relative "collection"
 require_relative "graphs"
 require_relative "viz"
 require_relative "viz_util"
--- lib/bud/collection.rb
+++ lib/bud/collection.rb
@@ -2,14 +2,24 @@
 module Bud
 
   # Add the rule from which the push element has been created
-  class PushElement < BudCollection
+  class PushElement
 
-    def initialize(name_in, bud_instance, collection_name, given_schema, defer_schema, & blk)
-      super
+    def initialize(name_in, bud_instance, collection_name=nil, given_schema=nil, defer_schema=false, &blk)
+      super(name_in, bud_instance, given_schema, defer_schema)
+      @blk = blk
+      @outputs = Set.new
+      @pendings = Set.new
+      @deletes = Set.new
+      @delete_keys = Set.new
+      @wired_by = []
+      @elem_name = name_in
+      @found_delta = false
+      @collection_name = collection_name
+      @invalidated = true
+      @rescan = true
+      # ### WLBud:Begin adding to Bud
       @orig_rule_id = bud_instance.current_eval_rule_id
-      if @orig_rule_id
-        raise WLError, "a PushElement has not received its rule_id of provenance"
-      end
+      raise WLBud::WLError, "a PushElement has not received its rule_id of provenance" unless @orig_rule_id
     end
 
   end
